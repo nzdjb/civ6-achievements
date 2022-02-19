d4a47d47e0048227fffadaf0da004e9750e9b66d
nzdjb <github@2a1b.com>
2022-02-19 00:08:10 +0000

    refactor: Parameterise app id.

--- civ6_achievements/__main__.py
+++ civ6_achievements/__main__.py
@@ -19,28 +19,28 @@ def create_app():
         player_data = {}
         if(is_valid_user(steam_id)):
             api = API_BASE + 'ISteamUserStats/GetPlayerAchievements/v0001'
-            user_resp = requests.get(api, params=params() | {'steamid': steam_id})
+            user_resp = requests.get(api, params=params(APP_ID) | {'steamid': steam_id})
             player_data = create_map('apiname', loads(user_resp.text).get('playerstats').get('achievements'))
         return dumps([{'apiname': k} | v | STATS_MAP[k] | player_data.get(k, {}) for k, v in SCHEMA_MAP.items()])
 
     return app
 
-def schema_map():
+def schema_map(app_id):
     api = API_BASE + 'ISteamUserStats/GetSchemaForGame/v0002'
-    schema_resp = requests.get(api, params=params())
+    schema_resp = requests.get(api, params=params(app_id))
     schema = loads(schema_resp.text).get('game').get('availableGameStats').get('achievements')
     return create_map('name', schema)
 
-def stats_map():
+def stats_map(app_id):
     api = API_BASE + "ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002"
-    stats_resp = requests.get(api, params={'gameid': APP_ID})
+    stats_resp = requests.get(api, params={'gameid': app_id})
     stats = loads(stats_resp.text).get('achievementpercentages').get('achievements')
     return create_map('name', stats)
 
-def params():
+def params(app_id):
     return {
         'key': STEAM_API_KEY,
-        'appid': APP_ID
+        'appid': app_id
     }
 
 def is_valid_user(user):
@@ -52,5 +52,5 @@ def create_map(index, list):
 API_BASE = 'http://api.steampowered.com/'
 APP_ID = '289070'
 STEAM_API_KEY = getenv('STEAM_API_KEY')
-SCHEMA_MAP = schema_map()
-STATS_MAP = stats_map()
+SCHEMA_MAP = schema_map(APP_ID)
+STATS_MAP = stats_map(APP_ID)

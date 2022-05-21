b7538fd4e0cb484072892ce7124ff21f493c8e18
nzdjb <github@2a1b.com>
2022-05-21 09:43:50 +0000

    style: Various fixes.

--- src/achievements/main.py
+++ src/achievements/main.py
@@ -9,11 +9,15 @@ import requests
 
 import boto3
 
-def handler(event, context):
+
+def handler(event, _):
     parameters = event.get('pathParameters')
     if not parameters or not parameters.get('app_id'):
         return response(400, 'No.')
-    message = achievements(parameters.get('app_id'), parameters.get('steam_id', None))
+    message = achievements(
+        parameters.get('app_id'),
+        parameters.get('steam_id', None)
+    )
     return response(message=message)
 
 
@@ -26,9 +30,24 @@ def achievements(app_id, steam_id=None):
     player_data = {}
     if(is_valid_user(steam_id)):
         api = API_BASE + 'ISteamUserStats/GetPlayerAchievements/v0001'
-        user_resp = requests.get(api, params=params(app_id) | {'steamid': steam_id})
-        player_data = create_map('apiname', loads(user_resp.text).get('playerstats').get('achievements'))
-    return dumps([{'apiname': k} | v | stats_map(app_id)[k] | player_data.get(k, {}) for k, v in schema_map(app_id).items()])
+        user_resp = requests.get(
+            api,
+            params=params(app_id) | {'steamid': steam_id}
+        )
+        player_data = create_map(
+            'apiname',
+            loads(user_resp.text).get('playerstats').get('achievements')
+        )
+    return dumps(
+        [
+            {'apiname': k}
+            | v
+            | stats_map(app_id)[k]
+            | player_data.get(k, {})
+            for k, v
+            in schema_map(app_id).items()
+        ]
+    )
 
 
 @cache
@@ -36,15 +55,21 @@ def schema_map(app_id):
     api = API_BASE + 'ISteamUserStats/GetSchemaForGame/v0002'
     schema_resp = requests.get(api, params=params(app_id))
     print(schema_resp)
-    schema = loads(schema_resp.text).get('game').get('availableGameStats').get('achievements')
+    schema = loads(schema_resp.text) \
+        .get('game') \
+        .get('availableGameStats') \
+        .get('achievements')
     return create_map('name', schema)
 
 
 @cache
 def stats_map(app_id):
-    api = API_BASE + "ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002"
+    api = API_BASE + \
+        "ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002"
     stats_resp = requests.get(api, params={'gameid': app_id})
-    stats = loads(stats_resp.text).get('achievementpercentages').get('achievements')
+    stats = loads(stats_resp.text) \
+        .get('achievementpercentages') \
+        .get('achievements')
     return create_map('name', stats)
 
 
@@ -56,7 +81,7 @@ def params(app_id):
 
 
 def is_valid_user(user):
-    return user != None
+    return user is not None
 
 
 def create_map(index, list):
